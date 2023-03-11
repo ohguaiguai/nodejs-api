bd19bbc8cb0a5a07a78a158f8e74c6edb0613252
归尘 <864412219@qq.com>
2023-03-11 21:43:30 +0800

    chore: 删除key

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  secret: 'zhihu-jwt-secret', // 真实情况下不能把密码写在这，一般是通过环境变量传入
+  secret: '', // 真实情况下不能把密码写在这，一般是通过环境变量传入
   connectionStr:
-    'mongodb+srv://zhangxing:admin@zhihu.lqc5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    '',
 };
